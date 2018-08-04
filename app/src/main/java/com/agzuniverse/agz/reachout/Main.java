8cacfe01709019a9a4a3adad83ef7ceb6b088cf6
Aswin G <aswinganesh666@gmail.com>
2018-08-04 12:59:01 +0530

    Homescreen done

--- app/src/main/java/com/agzuniverse/agz/reachout/Main.java
+++ app/src/main/java/com/agzuniverse/agz/reachout/Main.java
@@ -1,22 +1,30 @@
 package com.agzuniverse.agz.reachout;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Main extends AppCompatActivity {
+public class Main extends Activity {
+    private CustomButton getHelpButton;
+    private CustomButton giveHelpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn = findViewById(R.id.btn);
-        btn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v){
-                startActivity(new Intent(Main.this, GetHelp.class));
-            }
-        });
+//        Button btn = findViewById(R.id.btn);
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v){
+//                startActivity(new Intent(Main.this, GetHelp.class));
+//            }
+//        });
+        getHelpButton = findViewById(R.id.getHelpBtn);
+        giveHelpButton = findViewById(R.id.giveHelpBtn);
+
+        getHelpButton.setButtonText("I WANT HELP");
+        giveHelpButton.setButtonText("I WANT TO HELP");
     }
 }
